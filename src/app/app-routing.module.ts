452bac71c0ed7b007f5f26e795d651ca695bc2f7
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2023-09-28 17:18:13 -0500

    index changes-- adding components routes -- adding routing butons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompetidoresComponent } from './components/competidores/competidores.component';
+import { CombatesComponent } from './components/combates/combates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'competidores',
+    component: CompetidoresComponent
+  },
+  {
+  path: 'combates',
+  component: CombatesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetidoresComponent } from './components/competidores/competidores.component';
+import { CombatesComponent } from './components/combates/combates.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CompetidoresComponent,
+    CombatesComponent
+  ]
+  ,
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    
+
+    MatPaginatorModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTabsModule,
+    MatInputModule,
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
